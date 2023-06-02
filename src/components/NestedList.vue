86264a5e47f55e6f4206f3633d03b64393da5916
Kacper Fila <kacperfila26@gmail.com>
2023-06-02 14:09:15 +0200

    Display Data

---
+++ src/components/NestedList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item.name }}
+      <NestedList :items="item.children" v-if="item.children" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'NestedList',
+  props: {
+    items: Array,
+  },
+  components: {
+    NestedList: {
+      // Rekurencyjne wywołanie komponentu
+      extends: this,
+    },
+  },
+};
+</script>
