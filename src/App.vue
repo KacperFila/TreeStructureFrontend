3f9ecc6601b2b6218889078b44a9332a7ce1b081
Kacper Fila <kacperfila26@gmail.com>
2023-06-05 20:47:20 +0200

    Dragability add

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div v-if="fetchedData.length > 0">
+      <NestedList :items="fetchedData" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios';
+import NestedList from './components/NestedList.vue';
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    NestedList,
+  },
+  data() {
+    return {
+      fetchedData: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get('https://localhost:44313/api/item')
+        .then((response) => {
+          this.fetchedData = response.data;
+          console.log(this.fetchedData);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
--- src/components/NestedList.vue
+++ src/components/NestedList.vue
@@ -1,23 +1,72 @@
+<!--suppress VueUnrecognizedSlot -->
 <template>
-  <ul>
-    <li v-for="item in items" :key="item.id">
-      {{ item.name }}
-      <NestedList :items="item.children" v-if="item.children" />
-    </li>
-  </ul>
+  <div class="container">
+  <Draggable
+    v-model="localItems"
+    item-key="id"
+    class="myList"
+    tag="ul">
+    <template #item="{element}">
+      <li>
+        {{ element.title }}
+        <NestedList class="NestedElement" :items="element.childItems"
+                    v-if="element.childItems && element.childItems.length > 0" />
+      </li>
+    </template>
+  </Draggable>
+  </div>
 </template>
 
 <script>
+import Draggable from 'vuedraggable';
+
 export default {
   name: 'NestedList',
-  props: {
-    items: Array,
-  },
   components: {
-    NestedList: {
-      // Rekurencyjne wywołanie komponentu
-      extends: this,
+    Draggable,
+  },
+  props: {
+    items: {
+      type: Array,
     },
+  },
+  data() {
+    return {
+      localItems: [],
+      drag: false,
+      draggableOptions: {
+        group: 'nested-list',
+      },
+    };
+  },
+  mounted() {
+    this.localItems = [...this.items]; // Tworzenie kopii tablicy items
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  width: fit-content;
+}
+
+li {
+  width: 100%;
+  background-color: #f2f2f2;
+  padding:10px;
+  border: black solid 2px;
+}
+
+.NestedElement > li{
+  background: red;
+  margin-left: 30px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
