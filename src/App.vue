e150bcf7877c8816a67413712428f57224958216
Kacper Fila <kacperfila26@gmail.com>
2023-06-06 18:18:11 +0200

    Display and Edit Add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <div v-if="fetchedData.length > 0">
-      <NestedList :items="fetchedData" />
+  <div class="container">
+    <div class="left">
+      <form @submit.prevent="submitForm">
+        <label for="title">Tytuł:
+          <input type="text" id="title" v-model="formData.Title" required>
+        </label>
+        <button type="submit">Wyślij</button>
+      </form>
+    </div>
+
+    <div class="right">
+      <div v-if="fetchedData.length > 0">
+        <NestedList :items="fetchedData" :button-index="0" :value="sortDirection" />
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +29,10 @@ export default {
   data() {
     return {
       fetchedData: [],
+      formData: {
+        Title: '',
+        ParentItemId: null,
+      },
     };
   },
   created() {
@@ -25,16 +40,58 @@ export default {
   },
   methods: {
     fetchData() {
-      axios
-        .get('https://localhost:44313/api/item')
+      axios.get('https://localhost:44313/api/item')
         .then((response) => {
           this.fetchedData = response.data;
           console.log(this.fetchedData);
         })
         .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            console.log('Nie znaleziono danych.');
+          }
+        });
+    },
+    submitForm() {
+      const data = {
+        Title: this.formData.Title,
+        ParentItemId: this.formData.ParentItemId !== '' ? this.formData.ParentItemId : null,
+      };
+
+      axios.post('https://localhost:44313/api/item', data)
+        .then((response) => {
+          console.log(response.data);
+          this.fetchData();
+          window.location.reload();
+        })
+        .catch((error) => {
           console.error(error);
         });
+
+      this.formData.Title = '';
+      this.formData.ParentItemId = null;
     },
   },
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  min-width: 70%;
+  margin: 0 auto;
+}
+
+.left, .right {
+  flex: 1;
+  padding: 20px;
+  justify-content: center;
+}
+
+.left {
+  background: red;
+}
+
+.right {
+  background: yellow;
+}
+</style>
--- src/components/NestedList.vue
+++ src/components/NestedList.vue
@@ -1,24 +1,65 @@
-<!--suppress VueUnrecognizedSlot -->
 <template>
+  <button class="mainSort" v-if="buttonIndex === 0 && !drag" @click="toggleSortDirection">
+    ASC/DESC
+  </button>
+
   <div class="container">
-  <Draggable
-    v-model="localItems"
-    item-key="id"
-    class="myList"
-    tag="ul">
-    <template #item="{element}">
-      <li>
-        {{ element.title }}
-        <NestedList class="NestedElement" :items="element.childItems"
-                    v-if="element.childItems && element.childItems.length > 0" />
-      </li>
-    </template>
-  </Draggable>
-  </div>
+
+      <Draggable
+        v-model="localItems"
+        item-key="id"
+        class="myList"
+        tag="ul"
+        :options="draggableOptions"
+      >
+        <template #item="{ element }">
+          <li>
+            <div class="liTop">
+              {{ element.title }}
+              <form @submit.prevent="editItem(element.id)">
+                <label for="parentItemId">Parent Item Id:
+                  <input type="text" id="parentItemId" v-model="parentItemId" required>
+                </label>
+
+                <label for="title">Title:
+                  <input type="text" id="title" v-model="title" required>
+                </label>
+                <button type="submit">Wyślij</button>
+              </form>
+
+            </div>
+
+            <div class ="liBottom">
+              <button @click="deleteItem(element.id)">Delete catalog</button>
+
+              <form @submit.prevent="submitForm(element.id)">
+                <label :for="'formTitle_' + element.id">New element:
+                  <input
+                    :id="'formTitle_' + element.id"
+                    type="text"
+                    v-model="formTitle"
+                    required
+                  />
+                </label>
+                <button type="submit">Add</button>
+              </form>
+
+              <NestedList
+                class="NestedElement"
+                :items="element.childItems"
+                :button-index="buttonIndex + 1"
+                v-model="localSortDirection"
+              />
+            </div>
+          </li>
+        </template>
+      </Draggable>
+    </div>
 </template>
 
 <script>
 import Draggable from 'vuedraggable';
+import axios from 'axios';
 
 export default {
   name: 'NestedList',
@@ -29,6 +70,14 @@ export default {
     items: {
       type: Array,
     },
+    buttonIndex: {
+      type: Number,
+      default: 0,
+    },
+    value: {
+      type: String,
+      default: 'asc',
+    },
   },
   data() {
     return {
@@ -36,11 +85,81 @@ export default {
       drag: false,
       draggableOptions: {
         group: 'nested-list',
+        ghostClass: 'drag-ghost',
+        chosenClass: 'drag-chosen',
+        removeOnSpill: true,
       },
+      localSortDirection: this.value,
     };
   },
+  watch: {
+    localSortDirection(newSortDirection) {
+      this.$emit('input', newSortDirection);
+    },
+  },
   mounted() {
-    this.localItems = [...this.items]; // Tworzenie kopii tablicy items
+    this.localItems = [...this.items];
+  },
+  methods: {
+    toggleSortDirection() {
+      this.localSortDirection = this.localSortDirection === 'asc' ? 'desc' : 'asc';
+      this.localItems.sort((a, b) => {
+        if (this.localSortDirection === 'asc') {
+          return a.title.localeCompare(b.title);
+        }
+        return b.title.localeCompare(a.title);
+      });
+    },
+    submitForm(parentItemId) {
+      const data = {
+        ParentItemId: parentItemId,
+        Title: this.formTitle,
+      };
+      axios
+        .post('https://localhost:44313/api/item', data)
+        .then((response) => {
+          console.log(response.data);
+          // Obsługa odpowiedzi
+        })
+        .catch((error) => {
+          console.error(error);
+          // Obsługa błędu
+        });
+      window.location.reload();
+      this.formTitle = '';
+    },
+    editItem(Id) {
+      const data = {
+        ParentItemId: this.parentItemId,
+        Title: this.title,
+      };
+      console.log('Wartość zmiennej Id:', Id);
+      axios
+        .put(`https://localhost:44313/api/item/${Id}`, data)
+        .then((response) => {
+          console.log(response.data);
+          // Obsługa odpowiedzi
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error(error);
+          // Obsługa błędu
+        });
+    },
+    deleteItem(itemId) {
+      axios
+        .delete(`https://localhost:44313/api/item/${itemId}`)
+        .then((response) => {
+          console.log(response.data);
+          // Obsługa odpowiedzi
+        })
+        .catch((error) => {
+          console.error(error);
+          // Obsługa błędu
+        });
+      window.location.reload();
+      this.formTitle = '';
+    },
   },
 };
 </script>
@@ -54,19 +173,31 @@ ul {
 }
 
 li {
-  width: 100%;
-  background-color: #f2f2f2;
-  padding:10px;
-  border: black solid 2px;
+  display: flex;
+  flex-direction: column;
 }
 
-.NestedElement > li{
-  background: red;
-  margin-left: 30px;
+.liTop, .liBottom {
+  width: 100%;
 }
+
+.liTop {
+  background-color: #f2f2f2;
+  padding: 10px;
+  border: black solid 1px;
+}
+
+.liBottom {
+  background-color: lightblue;
+  padding: 10px;
+  border: black solid 1px;
+  border-top:0;
+}
+
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 </style>
